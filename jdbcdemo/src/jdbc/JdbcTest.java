e413f9fe3c668c7fa8504e3410fe672cf8c3ab7d
liu_bo <1095796478@qq.com>
2018-10-09 18:00:41 +0800

    优化statement,校验sql安全

--- jdbcdemo/src/jdbc/JdbcTest.java
+++ jdbcdemo/src/jdbc/JdbcTest.java
@@ -12,15 +12,21 @@ public class JdbcTest {
 
     public static void main(String[] args){
         Connection connection = null;
-        Statement statement = null;
+        PreparedStatement statement = null;
         ResultSet resultSet = null;
         try {
             connection = JDBCUtil.getConnection();
             if (connection != null) {
                 //mysql查询语句
-                String sql = "SELECT name FROM test1";
-                statement = connection.createStatement();
-                resultSet = statement.executeQuery(sql);
+                String sql = "SELECT * FROM test1 where name = ?";
+//                statement = connection.createStatement();
+//                resultSet = statement.executeQuery(sql);
+
+                //预先校验sql语句 ? 对应的内容,不管后面传递什么进来,都把它看成字符串 or select
+                statement = connection.prepareStatement(sql);
+                //?对应的索引从1开始
+                statement.setString(1,"11");
+                resultSet = statement.executeQuery();
 //
                 while (resultSet.next()) {
                     System.out.println("用户名:" + resultSet.getString("name"));
---
+++ jdbcdemo/src/jdbc/JdbcTestTest1.java
@@ -0,0 +1,18 @@
+package jdbc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author: liubo
+ * @version: 2018/10/9 17:28
+ * @describe:
+ */
+public class JdbcTestTest1 {
+
+    @Test
+    public void main() {
+        JdbcTest.main(null);
+    }
+}
