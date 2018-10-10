dbfd148f9145803c70c50afbc2d89b01516baa10
liu_bo <1095796478@qq.com>
2018-10-10 15:58:23 +0800

    配置servlet请求，响应 解决乱码问题等

--- servlet/src/HelloServlet.java
+++ servlet/src/HelloServlet.java
@@ -1,8 +1,12 @@
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 
 /**
@@ -45,11 +49,42 @@ public class HelloServlet extends javax.servlet.http.HttpServlet {
 
     @Override
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
 
     }
 
     @Override
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        /**
+         * get请求request 解决乱码:
+         * 1.new String(str.getBytes("ISO-8859-1"),"UTF-8")
+         *
+         * 2. tomcat conf/server.xml 加上 URIEncoding="UTF-8"
+         *  <Connector port="8080" protocol="HTTP/1.1"
+         *         connectionTimeout="20000"
+         *         redirectPort="8443"
+         *         URIEncoding="UTF-8" />
+         */
+
+        ServletContext servletContext = getServletContext();
+        String namespace = servletContext.getInitParameter("namespace");
+        //获取绝对路径
+        String realPath = servletContext.getRealPath("");
+        InputStream inputStream = new FileInputStream(realPath);
+
+//        ServletOutputStream outputStream = response.getOutputStream();
+//        byte[] buffer = new byte[1024];
+//        int len = 0;
+//        while ((len = inputStream.read(buffer)) != -1) {
+//            outputStream.write(buffer,0,len);
+//        }
+//        inputStream.close();
+//        outputStream.close();
+
+        servletContext.setAttribute("count", 1);
+        servletContext.getAttribute("count");
+
 
         ServletConfig servletConfig = getServletConfig();
         String servletName = servletConfig.getServletName();
@@ -63,6 +98,21 @@ public class HelloServlet extends javax.servlet.http.HttpServlet {
             }
             System.out.println("key ==  "+key+"   value ==  "+value);
         }
+
+//        response.setStatus(302);
+//        response.setHeader();
+//        response.setContentType();
+//        //默认编码是iso-8859-1
+//        response.setCharacterEncoding("UTF-8");
+//
+//        //规定浏览器使用此编码查看
+//        response.setHeader("Content-Type","text/html;charset=UTF-8");
+
+        //不管字节流还是字符流,都使用此段代码就行了
+        response.setContentType("text/html;charset=UTF-8");
+
+        response.getWriter().write("<h1>hello</h1>");
+//        response.getOutputStream().write("hello world".getBytes());
 
     }
 }
